d11e5355029f75678bf3fddc6d4af784be1fbfda
Avishai Taragin <79379047+48thFlame@users.noreply.github.com>
2023-09-04 17:46:20 +0300

    Main has a main_loop function - does as name

--- lib/main.exs
+++ lib/main.exs
@@ -6,15 +6,32 @@ defmodule Main do
   def term_dimensions do
     {:ok, rows_num} = :io.rows()
     {:ok, cols_num} = :io.columns()
-    {rows_num, cols_num}
+    {rows_num - 2, div(cols_num, 3)}
+  end
+
+  def main_loop(nil), do: :ok
+
+  def main_loop(aquarium, count) do
+    clear_screen()
+
+    Aquarium.display(aquarium) |> IO.puts()
+    Process.sleep(250)
+
+    if count <= 0 do
+      main_loop(nil)
+    else
+      main_loop(Aquarium.update(aquarium), count - 1)
+    end
   end
 
   def main do
-    rows_num = 6
-    cols_num = 8
+    rows_num = 8
+    cols_num = 12
 
     aquarium =
-      Aquarium.new({rows_num, cols_num}) |> Aquarium.display() |> IO.puts()
+      Aquarium.new({rows_num, cols_num})
+
+    main_loop(aquarium, cols_num)
   end
 end
 
